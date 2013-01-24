8734a3f452e3c2613572c08bed0501bbfec41d28
John McLear <john@mclear.co.uk>
2013-01-24 00:53:42 +0000

    working

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
-# See changes between revisions in Etherpad timeslider
+# Change the styling of a pad by passing a theme or styles in the URL as paramters
 
-![Alt text](http://i.imgur.com/5dqPPFj.png)
+Available Themes
+* hack
+* blackAndWhite
 
-Go away from document, want to see what's been changed since you left?
+Available Params
+theme     = Name of the theme IE hack or blackAndWhite
+font      = Pad font color
+bg        = Pad Background Color 
+toolbar   = Toolbar Background color
+sidebar   = Sidebar font color
+sidebarBg = Sidebar Background color
 
+# USAGE
+To use add the params to your Pad URL IE
+?theme=blackAndWhite
+http://beta.etherpad.org/p/test?theme=blackAndWhite
+
+![Alt text](http://i.imgur.com/TYO8qeh.png)
+
+# CONTRIBUTING
+See themes.js, use github.
+
+# INSTALL
 Install via the /admin/plugins UI in Etherpad Lite.
 
 # TODO
 
-* Lots...
-* Use SocketIO instead of AJAX (for streaming when searching through large pads)
-* UI polishing
-* Progress indicator
+* In UI setting to change Styles
+* EP Setting to set a theme by default
+* Use Cookies to save style settings
--- static/js/themes.js
+++ static/js/themes.js
@@ -1,29 +1,90 @@
-exports.themes = {
+exports.themes = function(hook, context){
 
-  init: function() {
-    var theme = themes.getUrlVars()['theme'];
-    if(theme){ // add the theme to the end of a document as a css link href
-      cssLink = "../../static/custom/" + theme + ".css";
-      cssLink = "<link rel='stylesheet' href='"+cssLink+"'>";
-      $('body').append(cssLink);
-    }
-  },
-
-  // Read a page's GET URL variables and return them as an associative array.
-  getUrlVars: function() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-      hash = hashes[i].split('=');
-      vars.push(hash[0]);
-      vars[hash[0]] = hash[1];
-    }
-    return vars;
+  if(!exports){
+    var exports = {};
   }
-}
 
-exports.postAceInit = function(hook, context){
+  exports.themes = {
+    init: function() {
+      var themes    = exports.themes;
+      var theme     = themes.getUrlVars()['theme'];
+      var font      = themes.getUrlVars()['font'];
+      var bg        = themes.getUrlVars()['bg'];
+      var toolbar   = themes.getUrlVars()['toolbar'];
+      var sidebar   = themes.getUrlVars()['sidebar'];
+      var sidebarBg = themes.getUrlVars()['sidebarBg'];
+
+      /* Themes config -- add more themes here*/
+      if(theme){
+        if(theme == "hack"){
+          themes.setBgColor("#000");
+          themes.setFontColor("#07C201");
+          themes.setToolbarColor("#000");
+          themes.setSideBarBgColor("#000");
+          themes.setSideBarFontColor("#07C201");
+        }
+        if(theme == "blackAndWhite"){
+          themes.setBgColor("#000");
+          themes.setFontColor("#FFF");
+          themes.setToolbarColor("#000");
+          themes.setSideBarBgColor("#000");
+          themes.setSideBarFontColor("#FFF");
+        }
+      }
+
+      if(font){
+        themes.setFontColor(font);
+      }
+      if(bg){
+        themes.setBgColor(bg);
+      }
+      if(toolbar){
+        themes.setToolbarColor(toolbar);
+      }
+      if(sidebar){
+        themes.setSideBarBgColor(sidebar);
+      }
+      if(sidebarBg){
+        themes.setSideBarFontColor(sidebarBg);
+      }
+
+    },
+
+    setBgColor: function(color){
+      $('iframe.[name="ace_outer"]').contents().find("#outerdocbody").css("background-color",color);
+    },
+
+    setFontColor: function(color){
+      $('iframe.[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").css("color",color);
+    },
+
+    setToolbarColor: function(color){
+      $('.toolbar').css("background",color);
+      $('.toolbar').css("background-color",color);
+    },
+
+    setSideBarBgColor: function(color){
+      $('iframe.[name="ace_outer"]').contents().find('#sidediv').css("background",color);
+      $('iframe.[name="ace_outer"]').contents().find('#sidediv').css("background-color",color);
+    },
+
+    setSideBarFontColor: function(color){
+      $('iframe.[name="ace_outer"]').contents().find('#sidedivinner').attr('style', 'color: '+color +' !important');
+    },
+
+  
+    // Read a page's GET URL variables and return them as an associative array.
+    getUrlVars: function() {
+      var vars = [], hash;
+      var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+      for(var i = 0; i < hashes.length; i++)
+      {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+      }
+      return vars;
+    }
+  }
   exports.themes.init();
 }
-
